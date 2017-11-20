4fe3fd104c9fc90f231dd8e00084c8e299d2590b
jpramassini <jpramassini@gmail.com>
2017-11-20 10:36:29 -0500

    Finished Exercise 3, time to test.

--- src/AirTravelCost.java
+++ src/AirTravelCost.java
@@ -1,3 +1,5 @@
+import java.util.GregorianCalendar;
+
 public class AirTravelCost implements TravelCost{
 
     private String arrivalDate = null;
@@ -17,6 +19,38 @@ public class AirTravelCost implements TravelCost{
         this.travelFare = travelFare;
         this.hotelCost = hotelCost;
     }
+    @Override
+    public String getDestination() {
+        return destination;
+    }
 
+    @Override
+    public double getDuration() {
+        GregorianCalendar departure = new GregorianCalendar(Integer.parseInt(departureDate.substring(0,4)), Integer.parseInt(departureDate.substring(4,6)),
+                Integer.parseInt(departureDate.substring(6,8)), Integer.parseInt(departureTime.substring(0,2)), Integer.parseInt(departureTime.substring(2,4)));
+        GregorianCalendar arrival = new GregorianCalendar(Integer.parseInt(arrivalDate.substring(0,4)), Integer.parseInt(arrivalDate.substring(4,6)),
+                Integer.parseInt(arrivalDate.substring(6,8)), Integer.parseInt(arrivalTime.substring(0,2)), Integer.parseInt(arrivalTime.substring(2,4)));
 
+        double departureTimeInMillis = departure.getTimeInMillis();
+        double arrivalTimeInMillis = arrival.getTimeInMillis();
+
+        return (arrivalTimeInMillis - departureTimeInMillis) / 60;
+    }
+
+    @Override
+    public double getTotalCost() {
+        return getLodgingCost() + travelFare + AGENT_FEE;
+    }
+
+    @Override
+    public double getLodgingCost() {
+        return getDuration()/24 * hotelCost;
+    }
+
+    @Override
+    public String toString(){
+        String output = "Air travel to " + getDestination() + " will take " + getDuration() + " hours and cost $" + String.format("%.2f",getTotalCost());
+
+        return output;
+    }
 }
--- src/TestCarTravelCost.java
+++ src/TestCarTravelCost.java
@@ -1,26 +1,56 @@
+import java.util.ArrayList;
+
 public class TestCarTravelCost {
 
     public static void main(String [] args){
         CarTravelCost car = null;
         TrainTravelCost train = null;
-        if(args.length == 5){
+        AirTravelCost plane = null;
+        if(args.length == 10){
             try{
                 car = new CarTravelCost(Integer.parseInt(args[0]),
                         Double.parseDouble(args[1]), args[2]);
                 train = new TrainTravelCost(Double.parseDouble(args[3]),
                         Double.parseDouble(args[4]), args[2]);
+                plane = new AirTravelCost(args[8], args[9], args[6], args[7], args[2],
+                        Double.parseDouble(args[5]), Double.parseDouble(args[1]));
             }
             catch(Exception e){
                 System.out.println("Invalid input type. Input arguments should be " +
-                        "(number of miles, hotel cost/night, destination, train duration, train fare).");
+                        "(number of miles, hotel cost/night, destination, train duration, train fare, air fare, " +
+                        "departure date, departure time, arrival date, arrival time).");
             }
+            ArrayList<TravelCost> travelCompare = new ArrayList<TravelCost>();
+            travelCompare.add(car);
+            travelCompare.add(train);
+            travelCompare.add(plane);
 
             System.out.println(car);
-
             System.out.println(train);
+            System.out.println(plane);
+
+            double lowestDuration = Double.POSITIVE_INFINITY;
+            double lowestCost = Double.POSITIVE_INFINITY;
+            int lowestCostIndex = 0;
+            int lowestDurationIndex = 0;
+            for (TravelCost item : travelCompare){
+
+                if (item.getDuration() < lowestDuration){
+                    lowestCost = item.getDuration();
+                    lowestDurationIndex = travelCompare.indexOf(item);
+                }
+
+                if (item.getTotalCost() < lowestCost){
+                    lowestCost = item.getTotalCost();
+                    lowestCostIndex = travelCompare.indexOf(item);
+                }
+            }
+
+            System.out.println("LOWEST COST: " + travelCompare.get(lowestCostIndex));
+            System.out.println("SHORTEST DURATION: " + travelCompare.get(lowestDurationIndex));
         }
         else{
-            System.out.println("Invalid number of command line arguments (You need 5)");
+            System.out.println("Invalid number of command line arguments (You need 10)");
         }
 
     }
